f85cf3a48fc13d8f2f4e441a0dfc0df1811cd755
Kasam <kasam.koolmind@gamil.com>
2023-12-18 17:16:43 +0530

    commit by kasam: custome chagnes

--- lib/src/stepper_page_view.dart
+++ lib/src/stepper_page_view.dart
@@ -1,4 +1,3 @@
-import 'package:either_dart/either.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -11,13 +10,8 @@ typedef StepperPageIndicatorBuilder = Widget Function(
   List<PageStep> pageSteps,
   PageControlsDetails pageControlsDetails,
   ValueListenable<double> pageProgress,
-);
-
-typedef StepperFormPageIndicatorBuilder = Widget Function(
-  BuildContext context,
-  List<FormPageStep> pageSteps,
-  PageControlsDetails pageControlsDetails,
-  ValueListenable<double> pageProgress,
+    VoidCallback? iconTap,
+    IconData? IconName,
 );
 
 typedef StepperPageItemBuilder = Widget Function(
@@ -26,29 +20,10 @@ typedef StepperPageItemBuilder = Widget Function(
   List<PageStep> pageSteps,
 );
 
-typedef StepperFormPageItemBuilder = Widget Function(
-  BuildContext context,
-  int currentPage,
-  List<FormPageStep> pageSteps,
-  GlobalKey<FormState> formKey,
-);
-
-typedef FormRequestNextPage = bool Function(
-  int currentPage,
-  List<FormPageStep> pageSteps,
-  List<GlobalKey<FormState>> formKeys,
-);
-
-// TODO: Let's redo this one more time where instead we can have the PageStep
-// be an either type of PageStep or FormPageStep... Perchance. (why are we
-// saying this???)
-
-// TODO:
-
 class StepperPageView extends StatefulWidget {
-  StepperPageView({
+  const StepperPageView({
     super.key,
-    required List<PageStep> pageSteps,
+    required this.pageSteps,
     this.pageController,
     this.initialPage,
     this.scrollDirection = Axis.horizontal,
@@ -57,37 +32,18 @@ class StepperPageView extends StatefulWidget {
     this.onPageChanged,
     this.pageTransitionDuration = const Duration(milliseconds: 300),
     this.pageTransitionCurve = Curves.easeInOut,
-    StepperPageIndicatorBuilder? headerIndicatorBuilder,
-    StepperPageItemBuilder? pageBuilder,
+    this.footerIndicatorBuilder,
+    this.headerIndicatorBuilder = defaultPageIndicatorBuilder,
+    this.pageBuilder = defaultPageBuilder,
     this.pagePadding = const EdgeInsets.all(0),
-  })  : headerIndicatorBuilder = const Left(defaultPageIndicatorBuilder),
-        pageSteps = Left(pageSteps),
-        pageBuilder = const Left(defaultPageBuilder),
-        onRequestNextPage = const Left(null);
-
-  StepperPageView.form({
-    super.key,
-    required List<FormPageStep> pageSteps,
-    this.pageController,
-    this.initialPage,
-    this.scrollDirection = Axis.horizontal,
-    this.reverse = false,
-    this.physics,
-    this.onPageChanged,
-    this.pageTransitionDuration = const Duration(milliseconds: 300),
-    this.pageTransitionCurve = Curves.easeInOut,
-    StepperFormPageIndicatorBuilder? headerIndicatorBuilder,
-    this.pagePadding = const EdgeInsets.all(0),
-    required FormRequestNextPage onRequestNextPage,
-  })  : headerIndicatorBuilder = const Right(defaultPageIndicatorBuilder),
-        pageSteps = Right(pageSteps),
-        pageBuilder = const Right(defaultFormPageBuilder),
-        onRequestNextPage = Right(onRequestNextPage);
+    this.iconTap,
+    this.iconName,
+  });
 
   /// The steps of the stepper whose titles, subtitles, icons always get shown.
   ///
   /// The length of [pageSteps] must not change.
-  final Either<List<PageStep>, List<FormPageStep>> pageSteps;
+  final List<PageStep> pageSteps;
 
   final PageController? pageController;
 
@@ -130,39 +86,45 @@ class StepperPageView extends StatefulWidget {
   /// Called whenever the page in the center of the viewport changes.
   final ValueChanged<int>? onPageChanged;
 
-  final Either<void, FormRequestNextPage> onRequestNextPage;
-
   /// The duration spent transitioning from page to page.
   final Duration pageTransitionDuration;
 
   /// The curve to use when animating from page to page.
   final Curve pageTransitionCurve;
 
+  /// A builder for the footer of the stepper.
+  final StepperPageIndicatorBuilder? footerIndicatorBuilder;
+
   /// A builder for the page indicator of the stepper.
-  final Either<StepperPageIndicatorBuilder, StepperFormPageIndicatorBuilder>
-      headerIndicatorBuilder;
+  final StepperPageIndicatorBuilder headerIndicatorBuilder;
 
   /// A builder for the content of the page.
   ///
   /// Override this if you need more customization than the default content
   /// field on a step can provider or if you need all pages to share some sort
   /// of common widget.
-  final Either<StepperPageItemBuilder, StepperFormPageItemBuilder> pageBuilder;
+  final StepperPageItemBuilder pageBuilder;
 
   final EdgeInsets pagePadding;
+
+  final VoidCallback? iconTap;
+
+  final IconData? iconName;
 
   static Widget defaultPageIndicatorBuilder(
     BuildContext context,
     List<PageStep> pageSteps,
     PageControlsDetails pageControlsDetails,
     ValueListenable<double> pageProgress,
+    VoidCallback? iconTap,
+    IconData? IconName,
   ) {
-    return SliverToBoxAdapter(
-      child: DefaultPageIndicator(
-        pageSteps: pageSteps,
-        pageControlsDetails: pageControlsDetails,
-        pageProgress: pageProgress,
-      ),
+    return DefaultPageIndicator(
+      pageSteps: pageSteps,
+      pageControlsDetails: pageControlsDetails,
+      pageProgress: pageProgress,
+      iconName: IconName,
+      iconTap: iconTap,
     );
   }
 
@@ -176,30 +138,6 @@ class StepperPageView extends StatefulWidget {
     return pageStep.content;
   }
 
-  static Widget defaultFormPageBuilder(
-    BuildContext context,
-    int index,
-    List<FormPageStep> pageSteps,
-    GlobalKey<FormState> formKey,
-  ) {
-    final formPageStep = pageSteps[index];
-
-    return Form(
-      key: formKey,
-      autovalidateMode: formPageStep.autovalidateMode,
-      onChanged: formPageStep.onChanged,
-      onWillPop: formPageStep.onWillPop,
-
-      /// This form allows the content to call Form.of(context) to do validation
-      /// and stuff.
-      child: Builder(
-        builder: (context) {
-          return formPageStep.content;
-        },
-      ),
-    );
-  }
-
   @override
   State<StepperPageView> createState() => _StepperPageViewState();
 }
@@ -208,7 +146,6 @@ class _StepperPageViewState extends State<StepperPageView> {
   late int currentStep;
   late PageController pageController;
   late final pageProgress = ValueNotifier<double>(0.0);
-  late final Either<void, List<GlobalKey<FormState>>> formKeys;
 
   /// We use this to know the direction a user is going.
   late ScrollDirection scrollDirection = ScrollDirection.forward;
@@ -216,22 +153,10 @@ class _StepperPageViewState extends State<StepperPageView> {
   @override
   void initState() {
     pageController = widget.pageController ??
-        PageController(initialPage: widget.initialPage ?? 0);
+        PageController(initialPage: widget.initialPage ?? 1);
     attachPageController(pageController);
-    currentStep = widget.initialPage ?? 0;
+    currentStep = widget.initialPage ?? 1;
     pageProgress.value = currentStep.toDouble();
-
-    formKeys = widget.pageSteps.fold(
-      (_) => const Left(null),
-      (formPageSteps) {
-        final formKeysToReturn = formPageSteps.map((formPageStep) {
-          return formPageStep.key ??
-              GlobalKey<FormState>(debugLabel: 'Title: ${formPageStep.title}');
-        }).toList();
-
-        return Right(formKeysToReturn);
-      },
-    );
 
     // The user provider a pageController and an initial page so should
     // set the page to their initial page. Argueably we shouldn't do anything.
@@ -259,32 +184,6 @@ class _StepperPageViewState extends State<StepperPageView> {
     if (widget.onPageChanged != oldWidget.onPageChanged) {
       pageController.removeListener(pageListener);
       pageController.addListener(pageListener);
-    }
-
-    final oldPageSteps = widget.pageSteps;
-    final newPageSteps = widget.pageSteps;
-
-    // Instead of erroring we can do some other logic where we just reset the
-    // form keys to the new length.
-    if (oldPageSteps.isLeft && newPageSteps.isRight) {
-      final newFormKeys = newPageSteps.right;
-      final oldFormKeys = oldPageSteps.left;
-
-      if (oldFormKeys.length != newFormKeys.length) {
-        throw Exception(
-          'The number of form keys must not change. '
-          'Old: ${oldFormKeys.length}, new: ${newFormKeys.length}',
-        );
-      }
-
-      for (var i = 0; i < oldFormKeys.length; i++) {
-        if (oldFormKeys[i] != newFormKeys[i]) {
-          throw Exception(
-            'The form keys must not change. '
-            'Old: ${oldFormKeys[i]}, new: ${newFormKeys[i]}',
-          );
-        }
-      }
     }
 
     super.didUpdateWidget(oldWidget);
@@ -329,7 +228,6 @@ class _StepperPageViewState extends State<StepperPageView> {
     if (widget.pageController == null) {
       cleanupPageController();
     }
-
     super.dispose();
   }
 
@@ -357,11 +255,8 @@ class _StepperPageViewState extends State<StepperPageView> {
 
   @override
   Widget build(BuildContext context) {
+    final footerBuilder = widget.footerIndicatorBuilder;
     final int stepIndex;
-    final pageStepsLength = widget.pageSteps.fold(
-      (pageSteps) => pageSteps.length,
-      (pageSteps) => pageSteps.length,
-    );
 
     switch (scrollDirection) {
       case ScrollDirection.idle:
@@ -389,124 +284,55 @@ class _StepperPageViewState extends State<StepperPageView> {
     // PersistentHeader as the page indicator instead? Maybe we could have
     // both? I would require SliverFillRemaining or whatever its called
     // for the body.
-    return SafeArea(
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          NestedScrollView(
-            floatHeaderSlivers: true,
-            headerSliverBuilder: (context, innerBoxIsScrolled) {
-              return [
-                SliverOverlapAbsorber(
-                  handle:
-                      NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-                  sliver: widget.headerIndicatorBuilder.fold(
-                    (pageBuilder) {
-                      final pageSteps = widget.pageSteps.left;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        widget.headerIndicatorBuilder(
+          context,
+          widget.pageSteps,
+          controlsDetails,
+          pageProgress,
+          widget.iconTap,
+          widget.iconName,
+        ),
+        Expanded(
+          // TODO: We need all the parameters of a PageView covered
+          // or we need to allow the user to create the PageView...
+          // One of the two
+          child: PageView.builder(
+            controller: pageController,
+            itemCount: widget.pageSteps.length,
+            onPageChanged: (index) {
+              setState(() => currentStep = index);
 
-                      return pageBuilder(
-                        context,
-                        pageSteps,
-                        controlsDetails,
-                        pageProgress,
-                      );
-                    },
-                    (formPageBuilder) {
-                      final formPageSteps = widget.pageSteps.right;
-
-                      return formPageBuilder(
-                        context,
-                        formPageSteps,
-                        controlsDetails,
-                        pageProgress,
-                      );
-                    },
-                  ),
-                ),
-              ];
+              widget.onPageChanged?.call(index);
             },
-            // TODO: We need all the parameters of a PageView covered
-            // or we need to allow the user to create the PageView...
-            // One of the two
-            body: PageView.builder(
-              controller: pageController,
-              itemCount: pageStepsLength,
-              onPageChanged: (index) {
-                setState(() => currentStep = index);
-
-                widget.onPageChanged?.call(index);
-              },
-              reverse: widget.reverse,
-              scrollDirection: widget.scrollDirection,
-              physics: widget.physics,
-              itemBuilder: (context, index) {
-                return CustomScrollView(
-                  slivers: [
-                    SliverOverlapInjector(
-                      handle: NestedScrollView.sliverOverlapAbsorberHandleFor(
-                        context,
-                      ),
-                    ),
-                    SliverFillRemaining(
-                      hasScrollBody: false,
-                      fillOverscroll: true,
-                      child: widget.pageBuilder.fold(
-                        (pageBuilder) {
-                          final pageSteps = widget.pageSteps.left;
-
-                          return pageBuilder(
-                            context,
-                            index,
-                            pageSteps,
-                          );
-                        },
-                        (formPageBuilder) {
-                          final formPageSteps = widget.pageSteps.right;
-                          final formKey = formKeys.right[index];
-
-                          return formPageBuilder(
-                            context,
-                            index,
-                            formPageSteps,
-                            formKey,
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                );
-              },
-            ),
-          ),
-          ...widget.onRequestNextPage.fold(
-            (_) => [],
-            (onRequestNextPage) {
-              return [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: FloatingActionButton(
-                      onPressed: () {
-                        final canContinue = onRequestNextPage(
-                          currentStep,
-                          widget.pageSteps.right,
-                          formKeys.right,
-                        );
-
-                        if (canContinue) {
-                          onNextPage();
-                        }
-                      },
-                      child: const Icon(Icons.arrow_forward),
-                    ),
-                  ),
+            reverse: widget.reverse,
+            scrollDirection: widget.scrollDirection,
+            physics: widget.physics,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: widget.pagePadding,
+                child: widget.pageBuilder(
+                  context,
+                  index,
+                  widget.pageSteps,
                 ),
-              ];
+              );
             },
           ),
-        ],
-      ),
+        ),
+        if (footerBuilder != null)
+          footerBuilder(
+            context,
+            widget.pageSteps,
+            controlsDetails,
+            pageProgress,
+            widget.iconTap,
+            widget.iconName,
+          ),
+      ],
     );
   }
 }
--- lib/src/widgets/default_page_indicator.dart
+++ lib/src/widgets/default_page_indicator.dart
@@ -13,6 +13,8 @@ class DefaultPageIndicator extends StatelessWidget {
     required this.pageSteps,
     required this.pageControlsDetails,
     required this.pageProgress,
+    this.iconName,
+    this.iconTap,
   });
 
   final List<PageStep> pageSteps;
@@ -21,109 +23,132 @@ class DefaultPageIndicator extends StatelessWidget {
 
   final ValueListenable<double> pageProgress;
 
+  final VoidCallback? iconTap;
+
+  final IconData? iconName;
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final currentStep = pageControlsDetails.currentStep;
     final onStepSelect = pageControlsDetails.onStepSelect;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Material(
-        borderRadius: BorderRadius.circular(32.0),
-        color: theme.colorScheme.primary.withAlpha(0x44),
-        elevation: 0.0,
-        child: Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: ValueListenableBuilder<double>(
-            valueListenable: pageProgress,
-            builder: (context, progress, _) {
-              // progress goes up to pageSteps.length
+    return Material(
+      // borderRadius: BorderRadius.circular(32.0),
+      color: Color(0xFFFDF0F2),
+      elevation: 0.0,
+      child: Padding(
+          padding: const EdgeInsets.only(left: 10.0, top: 5.0, bottom: 5.0),
+          child: Row(
+              children: [
+                IconButton(
+                  icon: Icon(
+                      iconName,
+                      color: Color(0xFFF22853)
+                  ),
+                  onPressed: iconTap,
+                ),
+                Expanded(
+                  child: Padding(
+                      padding: const EdgeInsets.only(left: 6.0, right: 16.0),
+                  child: ValueListenableBuilder<double>(
+                    valueListenable: pageProgress,
+                    builder: (context, progress, _) {
+                      // progress goes up to pageSteps.length
 
-              return Row(
-                mainAxisSize: MainAxisSize.max,
-                children: pageSteps.mapIndexed<Widget>(
-                  (index, step) {
-                    final maybeIcon = step.icon;
+                      return Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: pageSteps.mapIndexed<Widget>(
+                              (index, step) {
+                            final maybeIcon = step.icon;
 
-                    Widget iconChild;
+                            Widget iconChild;
 
-                    if (maybeIcon == null) {
-                      iconChild = Text('$index');
-                    } else {
-                      iconChild = maybeIcon;
-                    }
+                            final isPrevious = index < currentStep;
+                            final isNext = index > currentStep;
+                            final Color backgroundColor;
+                            final Color iconColor;
+                            final double elevation;
 
-                    final isPrevious = index < currentStep;
-                    final isNext = index > currentStep;
-                    final Color backgroundColor;
-                    final Color iconColor;
-                    final double elevation;
+                            if (maybeIcon == null) {
+                              iconChild = isPrevious == true
+                                  ? Icon(Icons.check_rounded, color: Colors.white)
+                                  : Text('${index + 1}', style: TextStyle(
+                                color: isNext == true ? Color(0xFF635D74) : Colors.white,
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.w700,),
+                              );
+                            } else {
+                              iconChild = maybeIcon;
+                            }
 
-                    if (isPrevious) {
-                      backgroundColor = theme.colorScheme.primary;
-                      iconColor = theme.colorScheme.onPrimary;
-                      elevation = 0.0;
-                    } else if (isNext) {
-                      backgroundColor = theme.colorScheme.surface;
-                      iconColor = theme.colorScheme.onSurface.withAlpha(140);
-                      elevation = 0.0;
-                    } else /* current */ {
-                      backgroundColor = theme.colorScheme.surface;
-                      iconColor = theme.colorScheme.onSurface;
-                      elevation = 12.0;
-                    }
+                            if (isPrevious) {
+                              backgroundColor = Color(0xFFF6A519);
+                              iconColor = Colors.white;
+                              elevation = 0.0;
+                            } else if (isNext) {
+                              backgroundColor = Color(0xFFE4E3E7);
+                              iconColor = Color(0xFF161616);
+                              elevation = 5.0;
+                            } else
+                              /* current */ {
+                              backgroundColor = Color(0xFFF22853);
+                              iconColor = Colors.white;
+                              elevation = 0.0;
+                            }
 
-                    return Material(
-                      shape: const CircleBorder(),
-                      elevation: elevation,
-                      color: backgroundColor,
-                      child: IconButton(
-                        padding: const EdgeInsets.all(4.0),
-                        constraints: const BoxConstraints(),
-                        color: iconColor,
-                        onPressed: onStepSelect == null
-                            ? null
-                            : () => onStepSelect(index),
-                        icon: iconChild,
-                      ),
-                    );
-                  },
-                ).intersperseIndexed(
-                  (int index) {
-                    final elementIndex = (index ~/ 2);
-                    final currentProgress = progress - elementIndex;
-
-                    return Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                        child: ShaderMask(
-                          shaderCallback: (Rect bounds) {
-                            return LinearGradient(
-                              colors: <Color>[
-                                theme.colorScheme.primary,
-                                theme.brightness == Brightness.dark
-                                    ? theme.colorScheme.onSurface
-                                    : theme.colorScheme.surface,
-                              ],
-                              stops: <double>[currentProgress, currentProgress],
-                            ).createShader(bounds);
+                            return Material(
+                              shape: const CircleBorder(),
+                              elevation: elevation,
+                              color: backgroundColor,
+                              child: IconButton(
+                                padding: const EdgeInsets.all(4.0),
+                                constraints: const BoxConstraints(),
+                                color: iconColor,
+                                onPressed: onStepSelect == null
+                                    ? null
+                                    : () => onStepSelect(index),
+                                icon: iconChild,
+                              ),
+                            );
                           },
-                          child: const DottedLine(
-                            dashRadius: 6.0,
-                            dashGapLength: 6.0,
-                            dashColor: Colors.white,
-                            lineThickness: 4.0,
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ).toList(),
-              );
-            },
-          ),
-        ),
+                        ).intersperseIndexed(
+                              (int index) {
+                            final elementIndex = (index ~/ 2);
+                            final currentProgress = progress - elementIndex;
+
+                            return Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                                child: ShaderMask(
+                                  shaderCallback: (Rect bounds) {
+                                    return LinearGradient(
+                                      colors: <Color>[
+                                        theme.colorScheme.primary,
+                                        theme.brightness == Brightness.dark
+                                            ? theme.colorScheme.onSurface
+                                            : theme.colorScheme.surface,
+                                      ],
+                                      stops: <double>[currentProgress, currentProgress],
+                                    ).createShader(bounds);
+                                  },
+                                  child: const DottedLine(
+                                    dashRadius: 6.0,
+                                    dashGapLength: 6.0,
+                                    dashColor: Color(0xFF8A8792),
+                                    lineThickness: 4.0,
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ).toList(),
+                      );
+                    },
+                  )),
+                )
+              ]
+          )
       ),
     );
   }
